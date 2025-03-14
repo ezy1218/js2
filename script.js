e8c6f5f29ed4c94d5bdf922f7c1c6df440385328
ezy1218 <ziyoda110@icloud.com>
2025-03-14 21:51:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// console.log('ismingizni kiriting')
+// alert('malumotingizni kiritganiz uchun tashakur')
+
+// var box ="nozi boq"
+// console.log(box)
+
+// let age = 25;
+// age = 30;
+// console.log(age);
+
+// const pi = 3.14;
+
+let type = prompt('sizning typedagi malumotingizni kiriting')
+console.log(type)
+
+let number = prompt('sizning number typedagi malumotingizni kiriting')
+console.log(number)
+
+let boolean = prompt('boolean typedagi malumotingizni kiriting')
+console.log(boolean)
+
+
+alert('malumotingizni togri kiritaganizga ishonchingiz komilmi?')
+
+let namee = "Akmaljon";
+let numberr = 123;
+let booleann = true;
+
+        let message = 
+    `Siz kiritgan ${namee} sozi string typega tegishli\n` +
+    `Siz kiritgan ${numberr} sozi number typega tegishli\n` +
+    `Siz kiritgan ${booleann} sozi boolean typega tegishli`;
+    
+
+alert(message);
+
+
+
+// let name1 = "Alex";  // двойные кавычки
+// let name2 = 'John';  // одинарные кавычк
+// let name3 = `Привет, ${name1}`; // обратные кавычки
+// console.log(name3); // "Привет, Alex"
